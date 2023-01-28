a2388669f7ad44a2ac516a71c04139890844c059
sergey-komar <kursev81@yandex.ru>
2023-01-28 11:33:51 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,25 @@
 $(function () {
 
+   $('.slider__big').slick({
+        slidesToShow: 1,
+		slidesToScroll: 1,
+		asNavFor: '.slider__thumb',
+        prevArrow: false,
+		nextArrow: false,
+        fade: true,
+        dots: true
+   }) 
+
+   $('.slider__thumb').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    asNavFor: '.slider__big',
+    prevArrow: false,
+	nextArrow: false
+}) 
 })
+
+
 const menu = document.querySelector('.menu');
     const mobile = document.querySelector('.nav-icon');
 
